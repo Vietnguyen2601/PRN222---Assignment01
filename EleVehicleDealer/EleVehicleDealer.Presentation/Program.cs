b686c436a4a73550a4a22292248e89e06be52562
lloocc11 <locttptse184628@fpt.edu.vn>
2025-09-26 00:06:41 +0700

    CssHome page

--- EleVehicleDealer/EleVehicleDealer.Presentation/Program.cs
+++ EleVehicleDealer/EleVehicleDealer.Presentation/Program.cs
@@ -43,6 +43,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}");
+    pattern: "{controller=Vehicle}/{action=Home}/{id?}");
 
 app.Run();
